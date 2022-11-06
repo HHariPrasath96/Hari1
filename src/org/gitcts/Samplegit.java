0b21a4dfa850c5c5448dc4a8e157b7eea7a1d20c
HHariprasath96 <hariprasathalagesan@gmail.com>
2022-11-06 18:42:34 +0530

    My Task

---
+++ src/org/gitcts/Samplegit.java
@@ -0,0 +1,39 @@
+package org.gitcts;
+
+public class Samplegit {
+	
+	private void method() {
+           System.out.println("java");
+	}
+	
+	
+	private void method2() {
+       System.out.println("Pithon");
+       
+	}
+	
+	
+	private void method3() {
+         System.out.println("C#");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
